bea1e73aa7eb619d3cc0295c876774b319ee4485
foboke <52140772+foboke@users.noreply.github.com>
2022-05-21 12:58:27 +0100

    Update image_upload.py

--- image_upload.py
+++ image_upload.py
@@ -2,14 +2,14 @@ import streamlit as st
 
 
 def load_image():
-    uploaded_file = st.file_uploader(label='Pick an image to test')
+    uploaded_file = st.file_uploader(label='Upload the image of the chest X-ray')
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         st.image(image_data)
 
 
 def main():
-    st.title('Image upload demo')
+    st.title('Pneumonia detection interface')
     load_image()
 
 
