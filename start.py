56833006ef38d63e042c4215225fa0fed0237f11
foboke <52140772+foboke@users.noreply.github.com>
2022-05-26 14:41:52 +0100

    Update start.py

--- start.py
+++ start.py
@@ -17,24 +17,7 @@ def main():
     st.sidebar.text('OR')
     image_file_chosen = st.sidebar.selectbox('Select an existing image:', get_list_of_images())
     
-    image_file = None
-    if image_file_uploaded:
-        image_file = image_file_uploaded
-    elif image_file_uploaded and image_file_chosen:
-        image_file = image_file_uploaded
-    else:
-        image_file = image_file_chosen
-
-
-    if image_file_uploaded and image_file and st.sidebar.button('Load'):
-        image = get_opened_image(image_file)
-        with st.beta_expander('Selected Image', expanded = True):
-            st.image(image, use_column_width = True)
-    
-    if image_file_chosen and image_file and st.sidebar.button('Load'):
-        image = get_opened_image(os.path.join(PATH_TO_TEST_IMAGES, image_file))
-        with st.beta_expander('Selected Image', expanded = True):
-            st.image(image, use_column_width = True)
+   
 
 
 if __name__ == '__main__':
