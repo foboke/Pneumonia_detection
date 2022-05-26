561f43528246b283cb4128cc97cd2612033fb17c
foboke <52140772+foboke@users.noreply.github.com>
2022-05-26 14:45:53 +0100

    Update start.py

--- start.py
+++ start.py
@@ -3,18 +3,14 @@ import os
 import sys
 
 st.sidebar.title("Pneumonia detection Classifier Interface ")
-PATH_TO_TEST_IMAGES = './test_images/'
 
-def get_list_of_images():
-    file_list = os.listdir(PATH_TO_TEST_IMAGES)
-    return [str(filename) for filename in file_list if str(filename).endswith('.jpg')]
 
 
 
 def main():
     st.sidebar.subheader('Load image')
     image_file_uploaded = st.sidebar.file_uploader('Upload an image', type = 'jpg')
-    st.sidebar.text('OR')
+    
     
     
    
