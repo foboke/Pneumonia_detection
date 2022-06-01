d3e729440ee89b08b253cb5f0ce6a19be77faff2
foboke <52140772+foboke@users.noreply.github.com>
2022-06-01 12:14:24 +0100

    Create streamlit_start.py

---
+++ streamlit_start.py
@@ -0,0 +1,27 @@
+import streamlit as st 
+import tensorflow as tf 
+import numpy as np 
+from PIL import Image, ImageOps
+
+
+st.write(""" # Pneumonia detection on Chest X-Rray """)
+upload_file = st.sidebar.file_uploader("Upload Chest X-Ray", type="png")
+Generate_pred=st.sidebar.button("Diagnostic")
+model=tf.keras.models.load_model('malaria_cell.h5')
+
+def import_n_pred(image_data, model):
+    size = (150,150)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    img = np.asarray(image)
+    reshape=img[np.newaxis,...]
+    pred = model.predict(reshape)
+    return pred
+  
+  
+if Generate_pred:
+    image=Image.open(upload_file)
+    with st.beta_expander('Cell Image', expanded = True):
+        st.image(image, use_column_width=True)
+    pred=import_n_pred(image, model)
+    labels = ['Parasitized', 'Uninfected']
+    st.title("Prediction of image is {}".format(labels[np.argmax(pred)]))
