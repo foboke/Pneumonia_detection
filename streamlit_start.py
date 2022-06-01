9dd8bee6a22a5fd2596053934d5707a291ddc99a
foboke <52140772+foboke@users.noreply.github.com>
2022-06-01 13:37:49 +0100

    Update streamlit_start.py

--- streamlit_start.py
+++ streamlit_start.py
@@ -7,7 +7,7 @@ from PIL import Image, ImageOps
 st.write(""" # Pneumonia detection on Chest X-Rray """)
 upload_file = st.sidebar.file_uploader("Upload Chest X-Ray", type="jpeg")
 Generate_pred=st.sidebar.button("Diagnostic")
-model=tf.keras.models.load_model('useful_files/model_pneumonia.h5')
+model=tf.keras.models.load_model('useful_files/model_pneumonia_new.h5')
 
 def import_n_pred(image_data, model):
     size = (150,150, 1)
